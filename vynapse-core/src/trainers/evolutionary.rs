012c0eade7f43a838e6c3099bd0001177c3ec2d9
Capataina <ata02caner@gmail.com>
2025-07-25 17:31:23 +0100

    removed mutate and crossover from genomes as the mutation and crossover logic will be handled in the trainer

--- vynapse-core/src/trainers/evolutionary.rs
+++ vynapse-core/src/trainers/evolutionary.rs
@@ -1,14 +1,21 @@
-use crate::traits::{fitness::Fitness, genome::Genome, selection::Selection};
+use crate::traits::{
+    crossover::Crossover, fitness::Fitness, genome::Genome, mutation::Mutation,
+    selection::Selection,
+};
 
-pub struct EvolutionaryTrainer<G, F, S>
+pub struct EvolutionaryTrainer<G, F, S, M, C>
 where
     G: Genome,
     F: Fitness,
     S: Selection,
+    M: Mutation,
+    C: Crossover,
 {
     population: Vec<G>,
     fitness_function: F,
     selection_strategy: S,
+    mutation_operator: M,
+    crossover_operator: C,
     population_size: usize,
     max_generations: usize,
     mutation_rate: usize,
--- vynapse-core/src/traits/genome.rs
+++ vynapse-core/src/traits/genome.rs
@@ -5,6 +5,4 @@ use vynapse_common::Result;
 pub trait Genome: Clone + Debug {
     fn get_weights(&self) -> Vec<f32>;
     fn set_weights(&mut self, weights: Vec<f32>) -> Result<()>;
-    fn mutate(&mut self, mutation_rate: f32) -> Result<()>;
-    fn crossover(&self, other: &Self) -> Result<Self>;
 }
